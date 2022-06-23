43da46fc6c85fab74f4ce6e49a8cc730145be902
Sergey <belochuvach@gmail.com>
2022-06-23 21:40:04 +0300

    Глупый бот

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 public class DummyBot {
-        public static String answer(String question) {
+    public static String answer(String question) {
         String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";

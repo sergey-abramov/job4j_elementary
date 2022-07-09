3e15bbac774c8c4ccac40392ac1784135d9d0bab
Sergey <belochuvach@gmail.com>
2022-07-09 15:54:35 +0300

    Поиск индекса в диапазоне

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,17 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexInRange(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int i = start; i <= finish; i++) {
+            for (int j = 0; j < data.length; j++) {
+                if (data[j] == el) {
+                    rst = i;
+                    break;
+                }
+            }
+        }
+        return rst;
+    }
 }

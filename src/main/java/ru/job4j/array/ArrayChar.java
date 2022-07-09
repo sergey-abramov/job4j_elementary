bc3e73a911d3a7f025cac0ab8fa256953119123b
Sergey <belochuvach@gmail.com>
2022-07-09 10:01:58 +0300

    Слово начинается с ...

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,8 +4,8 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            if (pref[0] == word[0]) {
-                result = pref[i] == word[i];
+            if (pref[i] != word[i]) {
+                result = false;
             }
         }
         return result;
